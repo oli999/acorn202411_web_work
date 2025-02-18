0974c302e4ca8109195d3df22a12c525bcd1433b
kimgura <oli999@naver.com>
2025-02-18 15:06:55 +0900

    Step04_Thymeleaf 예제 완성함

--- Spring04_Thymeleaf/src/main/java/com/example/spring04/controller/TestController.java
+++ Spring04_Thymeleaf/src/main/java/com/example/spring04/controller/TestController.java
@@ -11,6 +11,77 @@ import com.example.spring04.dto.MemberDto;
 @Controller
 public class TestController {
 	
+	@GetMapping("/unescape")
+	public String unescape(Model model) {
+		//html 형식의 문자열을 template 페이지에 전달할 일도 발생한다.
+		String html="""
+			<ul>
+				<li>하나</li>
+				<li>두울</li>
+			</ul>
+		""";
+		
+		model.addAttribute("content", html);
+		return "test/unescape";
+		
+	}
+	
+	@GetMapping("/include")
+	public String include(Model model) {
+		
+		model.addAttribute("title", "오늘의 인사");
+		model.addAttribute("content", "또 만났군요!");
+		
+		return "test/include";
+	}
+	
+	@GetMapping("/javascript")
+	public String javascript(Model model) {
+		//로그인여부
+		model.addAttribute("isLogin", false);
+		//나이
+		model.addAttribute("age", 15);
+		//이름
+		model.addAttribute("name", "김구라");
+		
+		//회원 한명의 정보 
+		MemberDto dto=MemberDto.builder()
+				.num(1)
+				.name("김구라")
+				.addr("노량진")
+				.build();
+		// 해당 데이터를 Model 객체에 담고 
+		model.addAttribute("dto", dto);
+		
+		//DB 에서 select 한 결과라고 가정하자 
+		MemberDto dto1=MemberDto.builder().num(1).name("김구라").addr("노량진").build();
+		MemberDto dto2=MemberDto.builder().num(2).name("해골").addr("행신동").build();
+		MemberDto dto3=MemberDto.builder().num(3).name("원숭이").addr("상도동").build();
+		
+		// read only List 
+		List<MemberDto> list=List.of(dto1, dto2, dto3);
+		
+		//Model 객체에 "list" 라는 키값으로 담기
+		model.addAttribute("list", list);
+		
+		return "test/javascript";
+	}
+	
+	@GetMapping("/if")
+	public String ifTest(Model model) {
+		
+		//로그인여부
+		model.addAttribute("isLogin", false);
+		//나이
+		model.addAttribute("age", 15);
+		//점수
+		model.addAttribute("jumsu", 90);
+		//role
+		model.addAttribute("role", "gura");
+		
+		return "test/if";
+	}
+	
 	@GetMapping("/sequence")
 	public String sequence() {
 		

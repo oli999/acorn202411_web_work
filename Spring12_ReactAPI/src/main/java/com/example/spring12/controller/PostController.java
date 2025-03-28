96ca527ea4a2d6118376882ec62b6586a27fc273
kimgura <oli999@naver.com>
2025-03-28 16:33:43 +0900

    Spring12 에 Cross Origin 설정함

--- Spring12_ReactAPI/src/main/java/com/example/spring12/controller/PostController.java
+++ Spring12_ReactAPI/src/main/java/com/example/spring12/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.spring12.dto.PostDto;
 import com.example.spring12.entity.Post;
 import com.example.spring12.repository.PostRepository;
 
+@CrossOrigin(origins = "*")
 @RequestMapping("/v1")
 @RestController
 public class PostController {

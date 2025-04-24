29cb339be626b435e578fe37aa50ff95aae4deea
kimgura <oli999@naver.com>
2025-04-24 09:31:58 +0900

    Spring18_DockerTest 예제 추가함

---
+++ Spring18_DockerTest/src/main/java/com/example/spring18/dto/MemberDto.java
@@ -0,0 +1,12 @@
+package com.example.spring18.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MemberDto {
+	private int num;
+	private String name;
+	private String addr;
+}
---
+++ Spring18_DockerTest/src/main/java/com/example/spring18/repository/MemberDaoImpl.java
@@ -0,0 +1,71 @@
+package com.example.spring18.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.spring18.dto.MemberDto;
+
+// dao 를 bean 으로 만들기 위한 어노테이션 
+@Repository
+public class MemberDaoImpl implements MemberDao{
+	
+	// MyBatis 기반으로 DB 관련 작업을 하기 위한 핵심 의존객체를 DI 받는다.
+	@Autowired
+	private SqlSession session;
+
+	@Override
+	public List<MemberDto> getList() {
+		/*
+		 *  SqlSession 객체를 이용해서 회원 목록을 얻어온다.
+		 */
+		List<MemberDto> list=session.selectList("member.getList");
+		
+		return list;
+	}
+
+	@Override
+	public void insert(MemberDto dto) {
+		/*
+		 *  Mapper 의 namespace : member
+		 *  sql 의 id : insert
+		 *  parameterType : MemberDto
+		 */
+		session.insert("member.insert", dto);
+	}
+
+	@Override
+	public void update(MemberDto dto) {
+		/*
+		 * Mapper namespace : member
+		 * sql id : update
+		 * parameterType : MemberDto
+		 */
+		session.update("member.update", dto);
+	}
+
+	@Override
+	public void delete(int num) {
+		
+		session.delete("member.delete", num);
+	}
+
+	@Override
+	public MemberDto getData(int num) {
+		MemberDto dto=session.selectOne("member.getData", num);
+		return dto;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+

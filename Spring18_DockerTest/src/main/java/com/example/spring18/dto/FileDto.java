b31924a58640c9e21a2be5de73957dde2850a33b
kimgura <oli999@naver.com>
2025-04-29 09:25:35 +0900

    Spring18_DockerTest 예제 완성함

---
+++ Spring18_DockerTest/src/main/java/com/example/spring18/dto/FileDto.java
@@ -0,0 +1,27 @@
+package com.example.spring18.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FileDto {
+	private long id;
+	private String uploader;
+	private String title;
+	private String orgFileName;
+	private String saveFileName;
+	private long fileSize;
+	private String uploadedAt;
+	//파일 업로드 폼에 있는  input type="file" 의 name 속성의 value 와 필드명이 일치해야한다.
+	// <input type="file" name="myFile">
+	private MultipartFile myFile;
+}
+
+
+
+
+
+
